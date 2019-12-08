59f2875b62e1ba8affb82131911c041304625b93
nataliaezh <natalya_ezhkova@mail.ru>
2019-12-08 14:05:29 +0300

    Initial commit

---
+++ src/ru/geekbrains/pattern/Main1.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.pattern;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("pattern: homework");
+    }
+}
